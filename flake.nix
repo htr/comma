436a1330c79f62ee12fa255e0744f87a3f3af535
Hugo T R <hugo@linux.com>
2022-03-07 17:51:48 +0100

    enabled aarch64-linux support

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   let
     b = builtins;
     lib = nixpkgs.lib;
-    supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" ];
     forAllSystems = f: lib.genAttrs supportedSystems
       (system: f system (import nixpkgs { inherit system; }));
   in
